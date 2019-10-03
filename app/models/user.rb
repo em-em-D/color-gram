3e474e23289d78c3b8400757c2ceff234d6cddd4
michaeldamulak <37841364+em-em-D@users.noreply.github.com>
2019-10-03 12:03:43 +0100

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,14 @@
 class User < ApplicationRecord
   has_many :post
   has_many :likes, dependent: :destroy
-  has_many :comment
+  has_many :comments, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :user_name, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, presence: true
-  validates :password, presence: true
+  validates :password, presence: true,
+                       format: { with: VALID_EMAIL_REGEX }
   has_many :images
   has_one_attached :avatar
 end
