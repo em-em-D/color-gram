63bc09808f3ab5592e8786ff1a9aea16b40ea407
em-em-D <michaeldamulak@gmail.com>
2019-11-04 18:36:52 +0100

    facebook login fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: %i[facebook]
+         :omniauthable, omniauth_providers: [:facebook]
   validates :email, presence: true
   validates :password, presence: true
   has_many :images
@@ -62,12 +62,19 @@ class User < ApplicationRecord
     confirmed_friends_posts + conirmed_inverse_friends_posts + posts
   end
 
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
-      user.password = devise.friendly_token[0, 20]
-      user.name = auth.info.name
-      user.image = auth.info.image
+      user.password = Devise.friendly_token[0,20]
+      user.username = auth.info.username   
     end
   end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require "omniauth-facebook"
 # :nodoc:
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
@@ -155,7 +156,6 @@ Devise.setup do |config|
 
   # Invalidates all the remember me tokens when the user signs out.
   config.expire_all_remember_me_on_sign_out = true
-  config.omniauth :facebook, 'APP_ID', 'APP_SECRET', token_params: { parse: :json }
   # If true, extends the user's remember period when remembered via cookie.
   # config.extend_remember_period = false
 
@@ -259,6 +259,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :facebook, "3038644956209663", "c2c25d92d0c992a9206c2ea088de85c1", callback_url: "http://localhost:3000/users/auth/facebook/callback"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
